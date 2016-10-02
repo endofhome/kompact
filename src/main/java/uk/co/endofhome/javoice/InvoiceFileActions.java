44fdbf241b17c733ccc707c135f3fb105fa8c08b
Tom Barnes <ruralmyth@gmail.com>
2016-10-02 22:11:25 +0100

    Refactoring - nicer implementation, uses order number for file name.

--- src/main/java/uk/co/endofhome/javoice/InvoiceFileActions.java
+++ src/main/java/uk/co/endofhome/javoice/InvoiceFileActions.java
@@ -2,9 +2,7 @@ package uk.co.endofhome.javoice;
 
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
 
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,27 +10,24 @@ import java.io.IOException;
 public class InvoiceFileActions {
     private final Invoice invoice;
     public final String rootPath;
-    private final NPOIFSFileSystem fs;
     public final HSSFWorkbook workBook;
     private final HSSFSheet sheet;
 
-    public InvoiceFileActions(Invoice invoice, File filePath) throws IOException {
+    public InvoiceFileActions(Invoice invoice, HSSFWorkbook workBook) {
         this.invoice = invoice;
         this.rootPath = "data/";
-        this.fs = new NPOIFSFileSystem(filePath);
-        this.workBook = new HSSFWorkbook(fs.getRoot(), true);
+        this.workBook = workBook;
         this.sheet = workBook.getSheetAt(2);
     }
 
     public Invoice writeFile(String filePath) throws IOException {
         try {
-            FileOutputStream fileOut = new FileOutputStream(filePath);
+            FileOutputStream fileOut = new FileOutputStream(filePath + invoice.orderNumber + ".xls");
             workBook.write(fileOut);
             fileOut.close();
         } catch (FileNotFoundException e) {
             throw new FileNotFoundException("There was a problem writing your file.");
         }
-        fs.close();
         return this.invoice;
     }
 }
--- src/test/java/uk/co/endofhome/javoice/InvoiceFileActionsTest.java
+++ src/test/java/uk/co/endofhome/javoice/InvoiceFileActionsTest.java
@@ -1,5 +1,9 @@
 package uk.co.endofhome.javoice;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
@@ -11,13 +15,29 @@ import static com.googlecode.totallylazy.matchers.Matchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class InvoiceFileActionsTest {
+    private Customer customer = new Customer("My Customer", "Some street", "Some town", "S0M3 T0W9", "1234567890", "ACC-01");
+    private Invoice invoice = new Invoice(LocalDate.now(), customer, "ORD-01", "Bob", sequence());
+    private InvoiceFileActions invoiceFileActions;
+    private File file;
+    private NPOIFSFileSystem fs;
+    private HSSFWorkbook workbook;
+
+    @Before
+    public void set_up() throws IOException {
+        file = new File("data/test.xls");
+        fs = new NPOIFSFileSystem(file);
+        workbook = new HSSFWorkbook(fs.getRoot(), true);
+        invoiceFileActions = new InvoiceFileActions(invoice, workbook);
+    }
+
+    @After
+    public void clean_up() throws IOException {
+        fs.close();
+    }
 
     @Test
     public void can_write_out_a_file() throws IOException {
-        Customer customer = new Customer("My Customer", "Some street", "Some town", "S0M3 T0W9", "1234567890", "ACC-01");
-        Invoice invoice = new Invoice(LocalDate.now(), customer, "ORD-01", "Bob", sequence());
-        InvoiceFileActions invoiceFileActions = new InvoiceFileActions(invoice, new File("data/test.xls"));
-        Invoice writtenInvoice = invoiceFileActions.writeFile(invoiceFileActions.rootPath + "testOutput.xls");
+        Invoice writtenInvoice = invoiceFileActions.writeFile(invoiceFileActions.rootPath);
         // TODO: read the written file and test some of the fields.
 
         assertThat(writtenInvoice.orderNumber, is("ORD-01"));
