b6c5161ec31b84945e4c455e7a3d4ce93440c996
Tom Barnes <ruralmyth@gmail.com>
2016-11-05 07:19:32 +1000

    reorganise Config class to make it easier to understand/update

--- src/main/java/uk/co/endofhome/javoice/Config.java
+++ src/main/java/uk/co/endofhome/javoice/Config.java
@@ -5,18 +5,28 @@ import java.nio.file.Paths;
 
 public class Config {
     private static Path defaultInvoiceFileOutputPath = Paths.get(String.format("%s/Javoice/Invoices", System.getProperty("user.home")));
+    private static Path defaultInvoiceFileTemplatePath = Paths.get(String.format("%s/Javoice/Invoices", System.getProperty("user.home")));
     private static Path defaultSalesLedgerFileOutputPath = Paths.get(String.format("%s/Javoice/Sales Ledger", System.getProperty("user.home")));
     private static Path defaultCustomerDataFileOutputPath = Paths.get(String.format("%s/Javoice/Customer Data", System.getProperty("user.home")));
 
-    public static Path defaultInvoiceFileOutputPath() {
-        return defaultInvoiceFileOutputPath;
+    private static Path invoiceFileOutputPath = defaultInvoiceFileOutputPath;
+    private static Path invoiceFileTemplatePath = defaultInvoiceFileTemplatePath;
+    private static Path salesLedgerFileOutputPath = defaultSalesLedgerFileOutputPath;
+    private static Path customerDataFileOutputPath = defaultCustomerDataFileOutputPath;
+
+    public static Path invoiceFileOutputPath() {
+        return invoiceFileOutputPath;
     }
 
-    public static Path defaultSalesLedgerFileOutputPath() {
-        return defaultSalesLedgerFileOutputPath;
+    public static Path invoiceFileTemplatePath() {
+        return invoiceFileTemplatePath;
     }
 
-    public static Path defaultCustomerDataFileOutputPath() {
-        return defaultCustomerDataFileOutputPath;
+    public static Path salesLedgerFileOutputPath() {
+        return salesLedgerFileOutputPath;
+    }
+
+    public static Path customerDataFileOutputPath() {
+        return customerDataFileOutputPath;
     }
 }
