18c7f30b7c0686cdfa9b61684c8385bf656a03de
Tom Barnes <tom.barnes@springer.com>
2016-12-09 12:05:38 +0000

    Controller can handle creation and recording of new invoices

--- src/main/java/uk/co/endofhome/javoice/Controller.java
+++ src/main/java/uk/co/endofhome/javoice/Controller.java
@@ -1,20 +1,67 @@
 package uk.co.endofhome.javoice;
 
+import com.googlecode.totallylazy.Sequence;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import uk.co.endofhome.javoice.customer.Customer;
 import uk.co.endofhome.javoice.customer.CustomerStore;
+import uk.co.endofhome.javoice.invoice.Invoice;
+import uk.co.endofhome.javoice.invoice.InvoiceClient;
+import uk.co.endofhome.javoice.invoice.ItemLine;
 import uk.co.endofhome.javoice.ledger.AnnualReport;
+import uk.co.endofhome.javoice.ledger.LedgerEntry;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.time.LocalDate;
 import java.time.Month;
+
+import static com.googlecode.totallylazy.Option.none;
+import static java.nio.file.Files.notExists;
+import static java.nio.file.Paths.get;
+import static uk.co.endofhome.javoice.Config.invoiceFileOutputPath;
+import static uk.co.endofhome.javoice.Config.salesLedgerFileOutputPath;
+import static uk.co.endofhome.javoice.invoice.InvoiceClient.invoiceClient;
+import static uk.co.endofhome.javoice.ledger.AnnualReport.annualReport;
+import static uk.co.endofhome.javoice.ledger.LedgerEntry.ledgerEntry;
 
 public class Controller {
     private CustomerStore customerStore;
 
-    public Controller(CustomerStore customerStore) {
+    Controller(CustomerStore customerStore) {
         this.customerStore = customerStore;
     }
 
-    public String nextInvoiceNumber(AnnualReport annualReport, Month month) {
+    void newInvoice(Customer customer, String orderNumber, Sequence<ItemLine> itemLines) throws IOException {
+        int year = LocalDate.now().getYear();
+        Month month = LocalDate.now().getMonth();
+        Path annualReportForThisYear = get(salesLedgerFileOutputPath().toString(), String.format("sales%s.xls", year));
+        if (notExists(annualReportForThisYear)) {
+            AnnualReport newAnnualReport = annualReport(year);
+            newAnnualReport.writeFile(salesLedgerFileOutputPath());
+        }
+        AnnualReport annualReport = AnnualReport.readFile(get(salesLedgerFileOutputPath().toString(), String.format("sales%s.xls", year)));
+        Invoice invoice = new Invoice(nextInvoiceNumber(annualReport, month), LocalDate.now(), customer, orderNumber, itemLines);
+        InvoiceClient invoiceClient = invoiceClient(new HSSFWorkbook());
+        HSSFSheet invoiceSheet;
+        try {
+            invoiceSheet = invoiceClient.getSingleSheetFromPath(Config.invoiceFileTemplatePath(), 2);
+        } catch (IOException e) {
+            throw new IOException("Controller could not get invoice template.");
+        }
+        invoiceClient.setSections(invoiceSheet, invoice);
+        try {
+            invoiceClient.writeFile(invoiceFileOutputPath(), invoice);
+        } catch (IOException e) {
+            throw new IOException("Controller could not write new invoice file.");
+        }
+        LedgerEntry ledgerEntry = ledgerEntry(invoice, none(), none(), none());
+        annualReport.setNewEntry(ledgerEntry);
+        annualReport.writeFile(salesLedgerFileOutputPath());
+    }
+
+    String nextInvoiceNumber(AnnualReport annualReport, Month month) {
         HSSFSheet sheetForThisMonth = annualReport.sheetAt(month.getValue());
         int lastInvoiceRowNum = sheetForThisMonth.getLastRowNum() -2;
         HSSFRow lastInvoiceRow = sheetForThisMonth.getRow(lastInvoiceRowNum);
--- src/test/java/uk/co/endofhome/javoice/ControllerTest.java
+++ src/test/java/uk/co/endofhome/javoice/ControllerTest.java
@@ -8,13 +8,15 @@ import uk.co.endofhome.javoice.invoice.Invoice;
 import uk.co.endofhome.javoice.invoice.ItemLine;
 import uk.co.endofhome.javoice.ledger.AnnualReport;
 import uk.co.endofhome.javoice.ledger.LedgerEntry;
+import uk.co.endofhome.javoice.ledger.MonthlyReport;
 
+import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 
 import static com.googlecode.totallylazy.Option.none;
 import static com.googlecode.totallylazy.Sequences.sequence;
-import static java.time.Month.NOVEMBER;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 import static uk.co.endofhome.javoice.ledger.AnnualReport.annualReportCustomConfig;
@@ -23,21 +25,42 @@ import static uk.co.endofhome.javoice.ledger.LedgerEntry.ledgerEntry;
 public class ControllerTest {
     private Controller controller;
     private AnnualReport annualReport;
+    private Path pathForTestOutput;
 
     @Before
-    public void set_up() {
+    public void set_up() throws IOException {
+        pathForTestOutput = Paths.get("src/test/resources/functional");
+        Config.setSalesLedgerFileOutputPath(pathForTestOutput);
+        Config.setInvoiceOutputPath(pathForTestOutput);
         CustomerStore customerStore = new CustomerStore();
         Customer customer = new Customer("some customer", null, null, null, null, null);
         controller = new Controller(customerStore);
         ItemLine itemLine = new ItemLine(5.0, "Slices of toast", 0.5);
-        Invoice invoice1 = new Invoice("1", LocalDate.of(2016, 11, 10), customer, "some ref", sequence(itemLine));
-        LedgerEntry ledgerEntry = ledgerEntry(invoice1, none(), none(), none());
-        annualReport = annualReportCustomConfig(2016, Paths.get("src/test/resources/functional"));
+        Invoice invoice = new Invoice("1", LocalDate.now(), customer, "some ref", sequence(itemLine));
+        LedgerEntry ledgerEntry = ledgerEntry(invoice, none(), none(), none());
+        annualReport = annualReportCustomConfig(LocalDate.now().getYear(), pathForTestOutput);
         annualReport.setNewEntry(ledgerEntry);
+        annualReport.writeFile(pathForTestOutput);
     }
 
     @Test
     public void can_get_next_invoice_number() {
-        assertThat(controller.nextInvoiceNumber(annualReport, NOVEMBER), is("2"));
+        assertThat(controller.nextInvoiceNumber(annualReport, LocalDate.now().getMonth()), is("2"));
+    }
+
+    @Test
+    public void can_create_a_new_invoice_and_write_out_all_files() throws Exception {
+        Customer anotherCustomer = new Customer("another customer", null, null, null, null, null);
+        ItemLine itemLine = new ItemLine(3.0, "Baked beans", 0.75);
+        controller.newInvoice(anotherCustomer, "another ref", sequence(itemLine));
+
+        AnnualReport annualReportFromFS = AnnualReport.readFile(Paths.get(pathForTestOutput.toString(), "sales" + String.valueOf(LocalDate.now().getYear()) + ".xls"));
+        int thisMonth = LocalDate.now().getMonthValue() -1;
+        MonthlyReport monthlyReport = annualReportFromFS.monthlyReports().get(thisMonth);
+        LedgerEntry lastEntry = monthlyReport.entries.last();
+        assertThat(lastEntry.invoiceNumber.get(), is("2"));
+        assertThat(lastEntry.date.get(), is(LocalDate.now()));
+        assertThat(lastEntry.customerName.get(), is("another customer"));
+        assertThat(lastEntry.valueNett.get(), is(2.25));
     }
 }
