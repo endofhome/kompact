5839ec8a3144f9a1165f423b250c23fdc6d2136c
Tom Barnes <tom.barnes@springer.com>
2017-07-07 15:19:47 +0100

    convert tricksy Observer and Observable interfaces into Kotlin
    I think, these are going to be short lived.

--- src/main/java/uk/co/endofhome/javoice/Observable.java
+++
@@ -1,9 +0,0 @@
-package uk.co.endofhome.javoice;
-
-import java.io.IOException;
-
-public interface Observable {
-    void registerObserver(Observer observer);
-    void newCustomer(String name, String addressOne, String addressTwo, String postcode, String phoneNumber) throws IOException;
-    void searchForCustomer(String name) throws Exception;
-}
